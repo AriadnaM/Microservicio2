a17a991481e0c5bb3d9dba26138c231cc822c63b
AriadnaM <ariadnamg12@gmail.com>
2021-01-15 14:51:48 -0600

    Se agrega code first

---
+++ Microservicio2/DAL/PersonaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservicio2.DAL
+{
+    public class PersonaRepository : IPersonaRepository
+    {
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
